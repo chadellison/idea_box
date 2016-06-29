843a971ad509b839a7fa82845779faa95ce91651
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-29 02:28:14 -0600

    Refactor quality function

--- app/assets/javascripts/requests.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".new-idea").on("ajax:success", function() {
-    $("form").trigger("reset");
-  }).on("ajax:error", function() {
-    // $(".new-idea").append "<p>ERROR</p>"
-  });
-});
