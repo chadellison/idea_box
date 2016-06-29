5b7c967f532be0d0a9ef2312be0f3d55600ea6a8
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-28 20:47:35 -0600

    Rebuild create without remotes

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,39 +2,25 @@ class Api::V1::IdeasController < Api::ApiController
   respond_to :json
 
   def index
-    # @ideas = Idea.order(created_at: :desc)
-    # @idea = Idea.new
     respond_with Idea.order(created_at: :desc)
   end
 
   def create
-    @idea = Idea.new(idea_params)
+    respond_with Idea.create(idea_params), location: nil
+  end
 
-    respond_to do |format|
-      if @idea.save
-        format.html { render :partial => @idea, notice: "Idea was successfully created." }
-        format.js
-        # binding.pry
-        # format.json { render json: @idea, status: :created, location: @idea }
-      else
-        # format.html { render action: "new" }
-        # format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
-    end
-
-    def destroy
-      Idea.find(params[:id]).destroy
-      # respond_to do |format|
-      #   format.html { render :partial => @idea }
-      #   format.js
-      # end
-      redirect_to root_path
-    end
+  def destroy
+    Idea.find(params[:id]).destroy
+    # respond_to do |format|
+    #   format.html { render :partial => @idea }
+    #   format.js
+    # end
+    redirect_to root_path
   end
 
   private
 
     def idea_params
-      params.require(:idea).permit(:title, :body)
+      params.require(:post).permit(:title, :body)
     end
 end
